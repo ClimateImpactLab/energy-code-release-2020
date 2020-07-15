8361d9cb6f3038b2b8c500f5ca4451137bc6ec9b
Ruixue Li <ruixueli@Epic-14934.local>
2020-07-15 18:19:32 -0500

    WIP

--- 3_post_projection/21jul2020presentation/get_32C_response_pct_climate_adaptation.R
+++ 3_post_projection/21jul2020presentation/get_32C_response_pct_climate_adaptation.R
@@ -4,6 +4,24 @@
 # Purpose: Calculates response functions for purposes of plotting responses and
 # visualizing temperature sensitivity (e.g., beta maps).  
 
+rm(list = ls())
+library(readr)
+library(dplyr)
+library(reticulate)
+library(parallel)
+library(miceadds)
+library(haven)
+library(ncdf4)
+library(tidyr)
+library(ggplot2)
+library(glue)
+library(data.table)
+
+source("/home/liruixue/projection_repos/mortality/2_projection/1_utils/load_utils.R")
+
+DB <- "/shares/gcp/estimation/mortality/release_2020/data"
+OUTPUT <- "/shares/gcp/estimation/mortality/release_2020/output"
+
 CSVV_DEFAULT = glue('{DB}/2_projection/3_impacts/main_specification/inputs/',
     'Agespec_interaction_GMFD_POLY-4_TINV_CYA_NW_w1.csvv')
 
@@ -171,12 +189,12 @@ calculate.beta = function(
     
     betas.expanded = left_join(betas.expanded, MMT, by = c("region"))
 
-    response = calculate_response(betas.expanded, temp, rel_temp, inc_adapt)
+    response = calculate_response(betas.expanded, temp, inc_adapt)
 
 
 
 
-#########################################
+################## want this part! #######################
 
     if (!is.null(summ_temp)) {
 
@@ -191,7 +209,7 @@ calculate.beta = function(
             response$decline = (b_2015 - inc_2100)/(b_2015 - full_2100)*100 
         }
 
-#########################################
+################### want this part! ######################
 
         # Store temperature distribution weighted average response to days above 30C in 
         # 2015 in Houston (Harris County USA.44.2628) and Seattle (King County USA.48.2971) 
@@ -247,6 +265,8 @@ calculate.beta = function(
 #' particular temperature value.
 #' @param inc_adapt Determins whether income adaptation is calculated (T/F)
 #' @return Dataframe containing clipped response functions.
+
+
 calculate_response = function( betas.expanded, temp, inc_adapt=F) {
 
     # Calculate response (full adaptation)
@@ -379,391 +399,4 @@ calculate_response = function( betas.expanded, temp, inc_adapt=F) {
     return(betas.expanded)
 }
 
-#' Plots maps providing that spatial distribution of the temperature sensitivity of
-#' mortality at a particular temperature value and year. In our analysis we call
-#' these objects "betas", though they represent the height of the response function
-#' at a given temperature value. (Figure 5)
-#' 
-#' Inputs
-#' ------
-#' This function requires an age group as well as any inputs to `calculate.beta`,
-#' though note that most important parameters for reproducing paper plots are
-#' defaults in both functions.
-#' 
-#' Outputs
-#' -------
-#' Exports "beta" maps to `outputwd`.
-#' 
-#' Dependencies
-#' -------------
-#' calculate.beta
-#' 
-#' Parameters/Return
-#' -----------------
-#' @param age agegroup ('young', 'older', 'oldest')
-#' @param limits_val list containing bounds for plotted maps by age group.
-#' @param yearlist years in which generate betas.
-#' @param grey_uclip T/F for diagnostic in which uclipped regions are greyed
-#' out.
-#' @param insample T/F for greying regions outside the estimation sample.
-#' @param outputwd Output directory
-#' @param shp_master Shapefile, will load default if NULL.
-#' @param betas.plot Betas dataframe (output from calculate.betas). Loads by
-#' default if NULL, but saves on compute time if running a bunch of output.
-#' @param summ_temp Temperature at which to calculate beta.
-#' @return Exports maps, returns NULL.
-plot.beta.maps = function(
-    age, 
-    limits_val=list(
-        young = 20,
-        older = 2,
-        oldest = 20 ),
-    yearlist=c(2015, 2050, 2100), 
-    grey_uclip=FALSE, 
-    insample=FALSE,
-    outputwd=BETA_OUTPUT_DEFAULT,
-    shp_master=NULL,
-    betas.plot=NULL,
-    summ_temp=35,
-    ...) {
 
-    if (is.null(betas.plot))
-        betas.plot = calculate.beta(age, yearlist=yearlist, summ_temp=summ_temp, ...)
-    message("Betas calculated. Plotting maps...")
-
-    # Load shapefile.
-    if (is.null(shp_master))
-        shp_master = load.map()
-
-    #join impact data to shapefile 
-    shp = left_join(shp_master, betas.plot, by=c('id' = 'region'))
-
-    for (yr in yearlist){
-        
-        #subset data to year
-        shp_plot = subset(shp, year==yr)
-
-        #set up plotting parameters. 
-        titlename = paste(yr,": Age", age)
-        
-        
-        #recode values that exceed limits_val to limits_val
-        shp_plot$betas_all_clip = ifelse(shp_plot$betas_all_clip > limits_val[[age]], 
-            limits_val[[age]], 
-            shp_plot$betas_all_clip)
-        
-        filename = glue("{outputwd}/beta_map_{age}_{yr}")
-        
-        if (grey_uclip) {
-            
-            #recode negative betas to NA
-            shp_plot$betas_all_clip = ifelse(
-                shp_plot$u_clipping == 1,
-                NA, shp_plot$betas_all_clip)
-            
-            # Update filename.
-            filename = glue("{outputwd}/beta_map_{age}_{yr}_grey-uclip")
-        }
-        
-        if (insample) {
-
-            insamplelist = c("BRA", "CHL", "CHN", "FRA", "JPN", "MEX", "USA",
-                "AUT", "BEL", "BGR", "CHE", "CYP", "CZE", "DEU", "DNK", "EST", 
-                "GRC", "ESP", "FIN", "HRV", "HUN", "IRL", "ISL", "ITA", "LIE", 
-                "LTU", "LUX", "LVA", "MNE", "MKD", "MLT", "NLD", "NOR", "POL", 
-                "PRT", "ROU", "SWE", "SVK", "TUR", "GBR")
-            
-            #recode outofsample IRs to NA
-            shp_plot$betas_all_clip = ifelse(!(substr(shp_plot$id, 1,3) %in% insamplelist), 
-                NA, shp_plot$betas_all_clip)
-            
-            # Update filename.
-            filename = paste0(filename,"_insample")
-        }
-        
-        # Set variables for plotting.
-        titleunit = glue("Damages at {summ_temp}C relative to reference temperature (deaths per 100,000)")
-        breaks_labels_val = round(seq(0, limits_val[[age]], limits_val[[age]]/4), digits = 5)
-        minval = min(shp_plot$betas_all_clip, na.rm=T) 
-        maxval = max(shp_plot$betas_all_clip, na.rm=T)
-        caption_val = glue("Min in {yr}: {minval}    Max in {yr}:  {maxval}    Grey: Out of Sample")
-        if(grey_uclip) 
-            caption_val = glue("Min in {yr}: {minval}    Max in {yr}:  {maxval}    Grey: u-clipped")
-        
-        message(paste("Plotting age", age, yr, sep=" "))
-
-        crs_str = "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-        lakeslist = list(
-            "CA-", "USA.23.1273","USA.14.642","USA.50.3082","USA.50.3083",
-            "USA.23.1275","USA.15.740", "USA.24.1355", "USA.33.1855", "USA.36.2089", 
-            "USA.23.1272", "UGA.32.80.484","UGA.32.80.484.2761", "TZA.13.59.1169", 
-            "TZA.5.26.564", "TZA.17.86.1759", "ATA")
-        lakes10 = ne_download(scale = 110, type = 'lakes', category = 'physical') %>%
-            spTransform(CRS(crs_str)) %>%
-            fortify(lakes10, region = "name")
-
-        #plot
-        p = ggplot(data = shp_plot, aes(x=long, y=lat)) +
-            geom_polygon(aes(group=group, fill=betas_all_clip)) +
-            geom_polygon(data = lakes10, aes(x=long, y=lat, group=group), fill="white") +
-            coord_equal() +
-            theme_bw() +     
-            theme(
-                plot.title = element_text(hjust=0.5, size = 10), 
-                plot.caption = element_text(hjust=0.5, size = 7), 
-                legend.title = element_text(hjust=0.5, size = 10), 
-                legend.position = "bottom",
-                legend.text = element_text(size = 7),
-                axis.title= element_blank(), 
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                panel.grid = element_blank(),
-                panel.border = element_blank()) +   
-            labs(
-                title = paste0("Mortality Betas at 35C ",titlename), 
-                caption = caption_val) + 
-            scale_fill_gradientn(
-                colors = rev(c("#c92116", "#ec603f", "#fd9b64",
-                    "#fdc370", "#fee69b","#fef7d1", "#f0f7d9")),
-                values = rescale(c(0, seq(0.000000001, limits_val[[age]], limits_val[[age]]/4))),
-                na.value = "grey80",
-                limits = c(0, limits_val[[age]]),
-                breaks = breaks_labels_val, 
-                labels = breaks_labels_val,
-                guide = guide_colorbar(
-                    title = titleunit,
-                    direction = "horizontal",
-                    barheight = unit(4, units = "mm"),
-                    barwidth = unit(180, units = "mm"),
-                    draw.ulim = F,
-                    title.position = 'top',
-                    title.hjust = 0.5,
-                    label.hjust = 0.5))
-        file_out = paste0(filename, ".png")
-        ggsave(p, file = file_out, width = 10, height = 6)
-        message(glue("Saved {file_out}"))
-
-    }
-}
-
-#' Plots unique response functions for all 25k impact regions, with alpha
-#' determined by that impact region's temperature distribution. Often referred to
-#' as "Spaghetti plots". (Figure 5)
-#' 
-#' Inputs
-#' ------
-#' This function requires an age group as well as any inputs to `calculate.beta`,
-#' though note that most important parameters for reproducing paper plots are
-#' defaults in both functions.
-#' 
-#' Outputs
-#' -------
-#' Exports "spaghetti" plots to `outputwd`.
-#' 
-#' Dependencies
-#' -------------
-#' calculate.beta
-#' 
-#' Parameters/Return
-#' -----------------
-#' @param age age agegroup ('young', 'older', 'oldest')
-#' @param insample T/F for greying regions outside the estimation sample.
-#' @param y.limits vector providing limits of the x-axis
-#' @param x.limits vector providing limits of the x-axis
-#' @param y.label y-axis label
-#' @param tempdir location of temperature data used to weight alpha.
-#' @param outputwd Output directory
-#' @param betas.plot dataframe (output from calculate.betas). Loads by
-#' default if NULL, but saves on compute time if running a bunch of output.
-#' @param ftype file type for output (png or pdf, usually)
-#' @return Exports plots, returns NULL.
-plot.spaghetti = function(
-    age='oldest', 
-    insample=FALSE, 
-    y.limits=c(0,15), 
-    x.limits=c(-15,40),
-    y.label="Change in death rate relative to reference temperature", 
-    tempdir=TEMP_DEFAULT,
-    outputwd=RF_OUTPUT_DEFAULT,
-    betas.plot=NULL,
-    ftype='pdf',
-    ... ) {
-
-    if (is.null(betas.plot))
-        betas = calculate.beta(age, baseline=2010,
-            yearlist=c(2010), summ_temp=NULL, ...)
-    
-    df = betas %>%
-        dplyr::select(hierid=region, betas_all_clip, temp) %>%
-        data.frame()
-
-    region = "Global"
-    if (insample) {
-
-        insamplelist = c("BRA", "CHL", "CHN", "FRA", "JPN", "MEX", "USA",
-            "AUT", "BEL", "BGR", "CHE", "CYP", "CZE", "DEU", "DNK", "EST", 
-            "GRC", "ESP", "FIN", "HRV", "HUN", "IRL", "ISL", "ITA", "LIE", 
-            "LTU", "LUX", "LVA", "MNE", "MKD", "MLT", "NLD", "NOR", "POL", 
-            "PRT", "ROU", "SWE", "SVK", "TUR", "GBR")
-
-        #recode outofsample IRs to NA
-        df$betas_all_clip = ifelse(!(substr(df$hierid, 1,3) %in% insamplelist), 
-            NA, df$betas_all_clip)
-
-        region="In-sample"
-    }
-
-    p = ggspaghetti(
-        df=df,
-        region=region,
-        variable="betas_all_clip", 
-        temp.variable="temp", 
-        key="hierid",
-        y.limits=y.limits,
-        x.limits=x.limits,
-        y.label=y.label )
-
-    ggsave(p, filename=glue("{outputwd}/{region}_spaghetti_response_{age}.{ftype}"),
-        width = 7, height = 7)  
-
-    return(p)
-}
-
-
-#' Plots maps providing the spatial distribution of the _change_ in the temperature
-#' sensitivity of mortality at a given temperature across two future years. Here
-#' we call these delta maps, but they're really delta of beta maps by the
-#' definition of beta above.
-#' 
-#' Inputs
-#' ------
-#' This function requires an age group as well as any inputs to `calculate.beta`,
-#' though note that most important parameters for reproducing paper plots are
-#' defaults in both functions.
-#' 
-#' Outputs
-#' -------
-#' Exports "delta" maps to `outputwd`.
-#' 
-#' Dependencies
-#' -------------
-#' calculate.beta
-#' 
-#' Parameters/Return
-#' -----------------
-#' @param  age agegroup ('young', 'older', 'oldest')
-#' @param limits_val list containing bounds for plotted maps by age group.
-#' @param baseline baseline year for counterfactual approximation (2015)
-#' @param future Future year from which to subtract baseline.
-#' @param outputwd Output directory
-#' @param shp_master Shapefile, will load default if NULL.
-#' @param betas.plot Betas dataframe (output from calculate.betas). Loads by
-#' default if NULL, but saves on compute time if running a bunch of output.
-#' @param summ_temp Temperature at which to calculate beta.
-#' @return Exports maps, returns NULL.
-plot.delta.maps = function(
-    age,
-    limits_val=list(
-        young = -6,
-        older = -2,
-        oldest = -20 ),
-    baseline=2015,
-    future=2100,
-    outputwd=BETA_OUTPUT_DEFAULT,
-    shp_master=NULL,
-    betas.plot=NULL,
-    summ_temp=35,
-    ...) {
-    
-    if (is.null(betas.plot))    
-        betas.plot = calculate.beta(age,
-            baseline=baseline,
-            yearlist=list(baseline, future), 
-            summ_temp=summ_temp, ...)
-    message("Betas calculated. Plotting maps...")
-
-    #reshape from long to wide
-    betas.reshape = dplyr::select(betas.plot, region, year, betas_all_clip) %>% 
-      tidyr::spread(year, betas_all_clip)
-    names(betas.reshape) = c("region", "baseline", "future")
-    
-    #calculate difference
-    betas.reshape$diff = betas.reshape$future -  betas.reshape$baseline
-    
-    # Load shapefile.
-    if (is.null(shp_master))
-        shp_master = load.map()
-
-    #join impact data to shapefile 
-    shp_plot = left_join(shp_master, betas.reshape, by = c('id' = 'region'))
-  
-    #set up plotting parameters
-    titlename = paste(future,": Age", age)
-    
-    #recode values that exceed limits_val to limits_val
-    shp_plot$diff = ifelse(
-        shp_plot$diff < limits_val[[age]],
-        limits_val[[age]], shp_plot$diff)
-    
-    filename = glue("{outputwd}/delta_map_{age}_{future}_sensitivity.png")
-    
-    # Set variables for plotting.
-    titleunit = glue("Change in marginal damages of a day at {summ_temp}C  (deaths per 100,000)")
-    breaks_labels_val = round(seq(limits_val[[age]], 0, 
-            abs(limits_val[[age]])/5), digits = 5)
-    minval = min(shp_plot$diff, na.rm=T)
-    maxval = max(shp_plot$diff, na.rm=T)
-    caption_val = glue("Min: {minval}    Max:  {maxval}")
-    
-    print(paste("plotting age", age, future, sep=" "))
-
-    crs_str = "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-    lakeslist = list(
-        "CA-", "USA.23.1273","USA.14.642","USA.50.3082","USA.50.3083",
-        "USA.23.1275","USA.15.740", "USA.24.1355", "USA.33.1855", "USA.36.2089", 
-        "USA.23.1272", "UGA.32.80.484","UGA.32.80.484.2761", "TZA.13.59.1169", 
-        "TZA.5.26.564", "TZA.17.86.1759", "ATA")
-    lakes10 = ne_download(scale = 110, type = 'lakes', category = 'physical') %>%
-        spTransform(CRS(crs_str)) %>%
-        fortify(lakes10, region = "name")
-    
-    #plot
-    p = ggplot(data = shp_plot, aes(x=long, y=lat)) +
-        geom_polygon(aes(group=group, fill=diff)) + # IR polygons
-        geom_polygon(data = lakes10, aes(x=long, y=lat, group=group), fill="white") +
-        coord_equal() +
-        theme_bw() +     
-        theme(
-            plot.title = element_text(hjust=0.5, size = 10), 
-            plot.caption = element_text(hjust=0.5, size = 7), 
-            legend.title = element_text(hjust=0.5, size = 10), 
-            legend.position = "bottom",
-            legend.text = element_text(size = 7),
-            axis.title= element_blank(), 
-            axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            panel.grid = element_blank(),
-            panel.border = element_blank()) +   
-        labs(
-            title = paste0("Sensitivity to high temperature ", future, "-", baseline), 
-            caption = caption_val) + 
-        scale_fill_gradientn(
-            colors = rev(c("#ffffbe", "#fffff7", "#fffff1",
-                "#D9F0A3", "#78C679", "#238443", "#006837")),
-            values = rescale(c(-20, -18, -16, -14, -12, -8, -4, 
-                -2, -1, -0.1, -0.000000001, 0, 0.000000001, 0.5, 2.5)), 
-            na.value = "grey80",
-            limits = c(limits_val[[age]], 3),
-            breaks = breaks_labels_val, 
-            labels = breaks_labels_val,
-            guide = guide_colorbar(
-                title = titleunit,
-                direction = "horizontal",
-                barheight = unit(4, units = "mm"),
-                barwidth = unit(180, units = "mm"),
-                draw.ulim = F,
-                title.position = 'top',
-                title.hjust = 0.5,
-                label.hjust = 0.5))
-    ggsave(p, file = filename, width = 10, height = 6)
-}
