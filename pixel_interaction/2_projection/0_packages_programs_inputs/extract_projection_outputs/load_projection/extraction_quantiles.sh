901f121713757efda541ad13991507b1fcea0eea
Ruixue Li <ruixueli@Epic-14934.local>
2020-07-31 19:49:40 +0800

    debug

--- pixel_interaction/2_projection/0_packages_programs_inputs/extract_projection_outputs/load_projection/extraction_quantiles.sh
+++ pixel_interaction/2_projection/0_packages_programs_inputs/extract_projection_outputs/load_projection/extraction_quantiles.sh
@@ -81,7 +81,7 @@ get_input_file() {
 		as_tag=-${as}
 	fi
 
-	stem=FD_FGLS_inter_clim${clim_data}_Exclude_all-issues_break2_${grouping_test}_poly2_${product}_${model}
+	stem=FD_FGLS_inter_OTHERIND_${product}_${model}
 	input_file=${stem}${as_tag}${price_scen_tag}${geo_level_tag}
 
         if [[ -z ${input_file} ]]; then
