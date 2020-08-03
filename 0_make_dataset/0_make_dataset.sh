3d426e66c289d7bfdd5960a290f9b242bc0ec88f
Ruixue Lui <liruixue@sacagawea.berkeley.edu>
2020-08-03 01:52:15 -0700

    0_make_dataset run

--- 0_make_dataset/0_make_dataset.sh
+++ 0_make_dataset/0_make_dataset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "STEP 1"
-stata-mp do -b 1_construct_dataset_from_raw_inputs.do
+#stata-mp  do 1_construct_dataset_from_raw_inputs.do
 echo "STEP 2"
-stata-mp do -b 2_construct_regression_ready_data.do
+stata-mp  do 2_construct_regression_ready_data.do
 echo "STEP 3"
-stata-mp do -b 3_unit_root_test_and_plot.do
+stata-mp  do 3_unit_root_test_and_plot.do
 echo "STEP 4"
 Rscript 4_plot_ITA_other_energy_regimes_timeseries.R
