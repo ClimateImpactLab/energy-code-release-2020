e31df290423dff1d3c9aee41e1e5b28fba0860a0
Ruixue Lui <liruixue@sacagawea.berkeley.edu>
2020-08-26 03:41:20 -0700

    done plots

--- 2_projection/2_running_projections/TINV_clim_aggregate_delta.sh
+++ 2_projection/2_running_projections/TINV_clim_aggregate_delta.sh
@@ -14,14 +14,14 @@ cd ${repo_root}/impact-calculations
 
 # Main model - aggregates both point estimate and delta method projections for all price scenarios
 
-for config in ${aggregate_config_path}/energy-aggregate-median-*electricity_dm.yml; do
+for config in ${aggregate_config_path}/energy-aggregate-median-*other_energy_dm.yml; do
 	echo "aggregating ${config}..." 
 	./aggregate.sh ${config} 66
 	sleep 4h
 done
 
 
-for config in ${aggregate_config_path}/energy-aggregate-median-*other_energy_dm.yml; do
+for config in ${aggregate_config_path}/energy-aggregate-median-*electricity_dm.yml; do
 	echo "aggregating ${config}..." 
 	./aggregate.sh ${config} 66
 	sleep 4h
