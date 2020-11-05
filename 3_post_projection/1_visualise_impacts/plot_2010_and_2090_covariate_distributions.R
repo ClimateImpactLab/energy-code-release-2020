d1030629134fb3309ade8e0da3df008473baf6ca
Ruixue Li <ruixueli@Epic-14934.local>
2020-11-05 23:33:57 +0800

    code for review

--- 3_post_projection/1_visualise_impacts/plot_2010_and_2090_covariate_distributions.R
+++ 3_post_projection/1_visualise_impacts/plot_2010_and_2090_covariate_distributions.R
@@ -15,12 +15,14 @@ pacman::p_load(ggplot2,
                scales, 
                grid)
 
+library(ggnewscale)
 DB = "/mnt"
 
 DB_data = paste0(DB, "/CIL_energy/code_release_data_pixel_interaction")
 root =  "/home/liruixue/repos/energy-code-release-2020"
 output = paste0(root, "/figures/fig_Appendix-C3_sample_overlap_present_future")
 dir.create(output, showWarnings = FALSE)
+source("/home/liruixue/projection_repos/post-projection-tools/mapping/imgcat.R") #this redefines the way ggplot plots. 
 
 covariates <- paste0(DB_data, "/projection_system_outputs/covariates/",
                      "/covariates-SSP3-rcp85-high-2010_2090-CCSM4.csv")
@@ -40,12 +42,18 @@ bin_num <- 50
 drop_val <- T
 
 
+# plot the red ones in reverse order for easier overlay
+covars_reverse = covars%>%arrange(desc(year))
+covars_reverse$order = factor(covars_reverse$year, levels = c(2090, 2010))
+
+
 # plot cdds both colors
 
 p_cdd = ggplot() +
   geom_bin2d(data=covars, 
              aes(x=CDD20, y=loggdppc), 
-             colour="white",
+             colour="transparent",
+             size = 1.2,
              bins = bin_num,
              drop = drop_val,
              na.rm = TRUE) +
@@ -58,18 +66,14 @@ p_cdd = ggplot() +
                        labels = breaks_labels_val,
                        values=rescale(rescale_val),
                        na.value=NA) +
-  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
-
-ggplot2::ggsave(width = 20, height = 10, plot = p_cdd, filename = paste0(output,'/Kdensity_CDD20_loggdppc_red.pdf'))
-
-p_cdd = ggplot() +
-  geom_bin2d(data=covars, 
+  new_scale_fill() +
+  geom_bin2d(data=covars_reverse, 
              aes(x=CDD20, y=loggdppc), 
-             colour="white",
+             # colour="white",
              bins = bin_num,
              drop = drop_val,
              na.rm = TRUE) +
-  facet_wrap(~year, nrow = 1) + 
+  facet_wrap(~order, nrow = 1) + 
   expand_limits(y=0) +
   scale_fill_gradientn(colours = grey.colors, 
                        name = "Frequency", 
@@ -80,19 +84,45 @@ p_cdd = ggplot() +
                        na.value=NA) +
   theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
 
+p_cdd
+
+ggplot2::ggsave(width = 20, height = 10, plot = p_cdd, filename = paste0(output,'/Kdensity_CDD20_loggdppc_red.pdf'))
+
+
+
+p_cdd = ggplot() +
+  geom_bin2d(data=covars_reverse, 
+             aes(x=CDD20, y=loggdppc), 
+             # colour="white",
+             bins = bin_num,
+             drop = drop_val,
+             na.rm = TRUE) +
+  facet_wrap(~order, nrow = 1) + 
+  expand_limits(y=0) +
+  scale_fill_gradientn(colours = grey.colors, 
+                       name = "Frequency", 
+                       limits = limits_val,  
+                       breaks = breaks_labels_val, 
+                       labels = breaks_labels_val,
+                       values=rescale(rescale_val),
+                       na.value=NA) +
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+
+
 ggplot2::ggsave(width = 20, height = 10, plot = p_cdd, filename = paste0(output,'/Kdensity_CDD20_loggdppc_gray.pdf'))
 
 
 # plot hdds both colors
 
 p_hdd = ggplot() +
-  geom_bin2d(data=covars, 
+  geom_bin2d(data=covars_reverse, 
              aes(x=HDD20, y=loggdppc), 
              colour="white",
+             size = 1.2,
              bins = bin_num,
              drop = drop_val,
              na.rm = TRUE) +
-  facet_wrap(~year, nrow = 1) + 
+  facet_wrap(~order, nrow = 1) + 
   expand_limits(y=0) +
   scale_fill_gradientn(colours = red.colors, 
                        name = "Frequency", 
@@ -108,7 +138,7 @@ ggplot2::ggsave(width = 20, height = 10, plot = p_hdd, filename = paste0(output,
 p_hdd = ggplot() +
   geom_bin2d(data=covars, 
              aes(x=HDD20, y=loggdppc), 
-             colour="white",
+             # colour="white",
              bins = bin_num,
              drop = drop_val,
              na.rm = TRUE) +
@@ -137,3 +167,10 @@ pop_2090 = sum(covars$population[covars$year ==2090], na.rm = TRUE)
 proportion = sum(covars$population[
   (covars$CDD20 < max_cdd_2010) & (covars$loggdppc < max_gdp_2010) & (covars$year ==2090)], na.rm = TRUE) / pop_2090
 proportion
+
+
+
+
+
+
+
--- adhoc_scripts/referee/time_series_by_FUND_regions.R
+++ adhoc_scripts/referee/time_series_by_FUND_regions.R
@@ -21,48 +21,11 @@ DB = "/mnt/CIL_energy"
 
 DB_data = paste0(DB, "/code_release_data_pixel_interaction")
 
-args = list(
-    conda_env = "risingverse-py27",
-    proj_mode = '', # '' and _dm are the two options
-    region = "global", # needs to be specified for 
-    rcp = "rcp85", 
-    ssp = "SSP3", 
-    price_scen = "price014", # have this as NULL, "price014", "MERGEETL", ...
-    unit =  "damage", # 'damagepc' ($ pc) 'impactpc' (kwh pc) 'damage' ($ pc)
-    uncertainty = "climate", # full, climate, values
-    geo_level = "aggregated", # aggregated (ir agglomerations) or 'levels' (single irs)
-    # iam = "high", 
-    model = "TINV_clim", 
-    adapt_scen = "fulladapt", 
-    clim_data = "GMFD", 
-    dollar_convert = "yes", 
-    yearlist = as.character(seq(1980,2100,1)),  
-    spec = "OTHERIND_total_energy",
-    grouping_test = "semi-parametric")
 
-args$iam = "high"
-df_high = do.call(load.median, args) %>% dplyr::select(year, mean)
-
-args$iam = "low"
-df_low = do.call(load.median, args) %>% dplyr::select(year, mean)
-
-p <- ggtimeseries(df.list = list(df_high, df_low), 
-                              df.x = "year",
-                              x.limits = c(2010, 2100),                               
-                              # y.limits=c(-0.8,0.2),
-                              y.label = "Total Damages, Billions of 2019 dollars", 
-                              legend.title = "Iam", legend.breaks = c("high", "low"), 
-                              legend.values = c('blue', 'red'))
-p
-
-
-
-# Get data for each fund region
-
+# a function to get data for each fund region
 # define a function to allow us to do a mapply festival 
 
 get_df = function(region, rcp, fuel, price_scen = NULL, unit = "impactpc", dollar_convert = NULL) {
-
 	print(paste0('------------------------------', region, '------------------------------'))
 
 	args = list(
@@ -86,139 +49,198 @@ get_df = function(region, rcp, fuel, price_scen = NULL, unit = "impactpc", dolla
 
     plot_df = do.call(load.median, c(args, region = region)) %>% 
                         dplyr::select(year, mean)
-    # names(plot_df) = c("year", region)
     return(plot_df)
 }
 
 
-plot_funds = function(rcp, aggregated_regions, fuel) {
 
-	df = mapply(get_df, region = aggregated_regions, rcp = rcp, fuel = fuel, SIMPLIFY = FALSE)
+# global FUND vs our main results in %GDP
 
-	# Get the required number of colors for plotting in the legend 
-	print('getting colors')
-	colourCount = length(aggregated_regions)
-	getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-	print('plotting')
-	p <- ggtimeseries(df.list = df, 
-	                              df.x = "year",
-	                              x.limits = c(2010, 2100),                               
-	                              # y.limits=c(-0.8,0.2),
-	                              y.label = paste0(fuel, " impactpc"), 
-	                              legend.title = "FUND-Region", 
-	                              legend.breaks = aggregated_regions, 
-	                              legend.values = getPalette(colourCount),
-	                              rcp.value = rcp, ssp.value = "SSP3", iam.value = "high") 
-	p
-	return(p)
-}
-
-
-aggregated_regions = c("global", "FUND-ANZ", "FUND-CAM", "FUND-CAN", "FUND-CHI", "FUND-EEU", "FUND-FSU", "FUND-JPK", "FUND-LAM", "FUND-MAF", "FUND-MDE", "FUND-SAS", "FUND-SEA", "FUND-SIS", "FUND-SSA", "FUND-USA", "FUND-WEU")
-x = plot_funds(rcp = "rcp85", aggregated_regions = aggregated_regions, fuel = "OTHERIND_other_energy")
-ggsave(x, file = '/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/FUND_other_energy_impactpc.png')
-y = plot_funds(rcp = "rcp85", aggregated_regions = aggregated_regions, fuel = "OTHERIND_electricity")
-ggsave(y, file = '/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/FUND_elec_impactpc.png')
-
-# get a csv of the damages, for ashwin: 
-get_and_name = function(rcp, region) {
-	args = list(
-		    conda_env = "risingverse-py27",
-		    proj_mode = '', # '' and _dm are the two options
-		    # region = "global", # needs to be specified for 
-		    rcp = rcp, 
-		    ssp = "SSP3", 
-		    price_scen = "price014", # have this as NULL, "price014", "MERGEETL", ...
-		    unit =  "damage", # 'damagepc' ($ pc) 'impactpc' (kwh pc) 'damage' ($ pc)
-		    uncertainty = "climate", # full, climate, values
-		    geo_level = "aggregated", # aggregated (ir agglomerations) or 'levels' (single irs)
-		    iam = "high", 
-		    model = "TINV_clim", 
-		    adapt_scen = "fulladapt", 
-		    clim_data = "GMFD", 
-		    dollar_convert = "yes", 
-		    yearlist = as.character(seq(1980,2100,1)),  
-		    spec = "OTHERIND_total_energy",
-		    grouping_test = "semi-parametric")
-	
-	df = do.call(load.median, c(args, region = region)) %>% 
-	                        dplyr::select(mean)
-	names(df) = c(region)
-	return(df)
-}
-
-save_csv = function(rcp) {
-	df = mapply(get_and_name, region = aggregated_regions, rcp = "rcp45", SIMPLIFY = FALSE) %>%
-			bind_cols()
-	df$year = seq(1981,2100,1)
-	write_csv(df, paste0('/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/damages-price014-',
-		rcp, 'total_energy-SSP3-high.csv'))
-	return(df)
-}
-y = save_csv(rcp = "rcp45")
-z = save_csv(rcp = "rcp85")
-
-
-# global FUND vs our main results in dollar values
-
+# load world GDP for SSP3 and FUND
 df_gdp = read_csv(paste0(DB_data, '/projection_system_outputs/covariates/', 
                        "/SSP3-global-gdp-time_series.csv"))
 
+df_gdp_fund = read_csv(paste0(REPO,"/energy-code-release-2020/data/", 
+                       "/FUND_GDP_bn1995USD.csv")) %>% 
+			group_by(time) %>%
+			summarise(gdp= sum(income)) %>%
+			rename(year = time) %>% 
+			dplyr::filter(year >= 2001, year <= 2100)
+
+# load the file used in mortality for dollar conversion
+# this value is not used for the %gdp calculation
+file_fed = read_csv('/shares/gcp/estimation/mortality/release_2020/data/3_valuation/inputs/adjustments/fed_income_inflation.csv')
+convert_1995_to_2019 = (file_fed %>% filter(year == 2019))$gdpdef / (file_fed %>% filter(year == 1995))$gdpdef 
 
 
+
+# load projected impacts in billion of 2019 dollar
 total_rcp85 = get_df(rcp = "rcp85", region = c("global"), fuel = "OTHERIND_total_energy", price_scen = "price014", unit = "damage", dollar_convert = TRUE) %>% 
-				# mutate(mean = mean / 1000000000) %>% 
 				dplyr::select(year, mean)
-
 total_rcp45 = get_df(rcp = "rcp45", region = c("global"), fuel = "OTHERIND_total_energy", price_scen = "price014", unit = "damage", dollar_convert = TRUE) %>%
-				# mutate(mean = mean / 1000000000) %>% 
 				dplyr::select(year, mean)
 
-# total = merge(total_rcp85, total_rcp45, by = "year")
 
-
-
-df = total_rcp85 %>%
+# calcualte percent gdp for SSP3 impacts
+df_85 = total_rcp85 %>%
   left_join(df_gdp, by = "year") %>% 
   mutate(mean = mean * 1000000000) %>% #convert from billions of dollars 
-  mutate(percent_gdp = (mean/gdp) *100 / 0.0036)
+  mutate(percent_gdp = (mean/gdp) *100 / 0.0036) %>% 
+  dplyr::select(year, percent_gdp)
 
+df_45 = total_rcp45 %>%
+  left_join(df_gdp, by = "year") %>% 
+  mutate(mean = mean * 1000000000) %>% #convert from billions of dollars 
+  mutate(percent_gdp = (mean/gdp) *100 / 0.0036) %>% 
+  dplyr::select(year, percent_gdp)
 
+# load FUND impacts in billion 1995 dollar
 FUND_cooling = read_csv(paste0(REPO,"/energy-code-release-2020/data/", "FUND_impacts_bn1995USD_cooling.csv"))
 FUND_heating = read_csv(paste0(REPO,"/energy-code-release-2020/data/", "FUND_impacts_bn1995USD_heating.csv"))
 
+# sum cooling and heating
 FUND_sum = merge(FUND_cooling, FUND_heating, by = c("time","regions")) %>% 
 	mutate(total_energy = cooling + heating) %>%
 	group_by(time) %>%
 	summarise(mean= sum(total_energy)) %>%
 	rename(year = time) %>% 
-	filter(year <= 2100, year >= 1981)
+	filter(year <= 2100, year >= 2001)
 
-# all_data = merge(FUND_sum, total, by = "year")
-# FUND_sum
+# calculate rebaser
+rebaser <- with(FUND_sum, mean(mean[year >= 2001 & year <= 2010]) )
 
-	# print('getting colors')
-	# colourCount = 3
+# calculate % GDP for FUND impacts
+df_fund = FUND_sum %>%
+  left_join(df_gdp_fund, by = "year") %>% 
+  mutate(mean = mean - rebaser) %>% #convert from billions of dollars 
+  mutate(percent_gdp = -(mean/gdp) *100)  %>% 
+  dplyr::select(year, percent_gdp)
+
+
+# plot
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-	# print('plotting')
-p <- ggtimeseries(df.list = list(total_rcp45 %>% as.data.frame() , 
-                total_rcp85 %>% as.data.frame(),
-                FUND_sum %>% as.data.frame()), 
+p <- ggtimeseries(df.list = list(df_45 %>% as.data.frame() , 
+                df_85 %>% as.data.frame(),
+                df_fund %>% as.data.frame()), 
                   df.x = "year",
-                  x.limits = c(1981, 2100),                               
+                  x.limits = c(2010, 2100),                               
                   # y.limits=c(-0.8,0.2),
-                  y.label = " impact-dollar", 
-                  legend.title = "FUND-Region", 
+                  y.label = "%GDP", 
+                  legend.title = "Model", 
                   legend.breaks = c("rcp45","rcp85","FUND"), 
                   legend.values = getPalette(3),
                   rcp.value = "rcp85", ssp.value = "SSP3", iam.value = "high") 
 
+ggsave(p, file = '/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/FUND_global_time_series.pdf')
 
-# p = plot_funds_new(df = all_data)
 
 
-# file_fed = read_csv('/shares/gcp/estimation/mortality/release_2020/data/3_valuation/inputs/adjustments/fed_income_inflation.csv')
 
-# convert_1995_to_2019 = (file_fed %>% filter(year == 2019))$gdpdef / (file_fed %>% filter(year == 1995))$gdpdef 
+################################ old code by tom, no need to review for now ###################################
+
+# args = list(
+#     conda_env = "risingverse-py27",
+#     proj_mode = '', # '' and _dm are the two options
+#     region = "global", # needs to be specified for 
+#     rcp = "rcp85", 
+#     ssp = "SSP3", 
+#     price_scen = "price014", # have this as NULL, "price014", "MERGEETL", ...
+#     unit =  "damage", # 'damagepc' ($ pc) 'impactpc' (kwh pc) 'damage' ($ pc)
+#     uncertainty = "climate", # full, climate, values
+#     geo_level = "aggregated", # aggregated (ir agglomerations) or 'levels' (single irs)
+#     # iam = "high", 
+#     model = "TINV_clim", 
+#     adapt_scen = "fulladapt", 
+#     clim_data = "GMFD", 
+#     dollar_convert = "yes", 
+#     yearlist = as.character(seq(1980,2100,1)),  
+#     spec = "OTHERIND_total_energy",
+#     grouping_test = "semi-parametric")
+
+# args$iam = "high"
+# df_high = do.call(load.median, args) %>% dplyr::select(year, mean)
+
+# args$iam = "low"
+# df_low = do.call(load.median, args) %>% dplyr::select(year, mean)
+
+# p <- ggtimeseries(df.list = list(df_high, df_low), 
+#                               df.x = "year",
+#                               x.limits = c(2010, 2100),                               
+#                               # y.limits=c(-0.8,0.2),
+#                               y.label = "Total Damages, Billions of 2019 dollars", 
+#                               legend.title = "Iam", legend.breaks = c("high", "low"), 
+#                               legend.values = c('blue', 'red'))
+# p
+
+
+
+
+# plot_funds = function(rcp, aggregated_regions, fuel) {
+
+# 	df = mapply(get_df, region = aggregated_regions, rcp = rcp, fuel = fuel, SIMPLIFY = FALSE)
+
+# 	# Get the required number of colors for plotting in the legend 
+# 	print('getting colors')
+# 	colourCount = length(aggregated_regions)
+# 	getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+# 	print('plotting')
+# 	p <- ggtimeseries(df.list = df, 
+# 	                              df.x = "year",
+# 	                              x.limits = c(2010, 2100),                               
+# 	                              # y.limits=c(-0.8,0.2),
+# 	                              y.label = paste0(fuel, " impactpc"), 
+# 	                              legend.title = "FUND-Region", 
+# 	                              legend.breaks = aggregated_regions, 
+# 	                              legend.values = getPalette(colourCount),
+# 	                              rcp.value = rcp, ssp.value = "SSP3", iam.value = "high") 
+# 	p
+# 	return(p)
+# }
+
+
+# aggregated_regions = c("global", "FUND-ANZ", "FUND-CAM", "FUND-CAN", "FUND-CHI", "FUND-EEU", "FUND-FSU", "FUND-JPK", "FUND-LAM", "FUND-MAF", "FUND-MDE", "FUND-SAS", "FUND-SEA", "FUND-SIS", "FUND-SSA", "FUND-USA", "FUND-WEU")
+# x = plot_funds(rcp = "rcp85", aggregated_regions = aggregated_regions, fuel = "OTHERIND_other_energy")
+# ggsave(x, file = '/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/FUND_other_energy_impactpc.png')
+# y = plot_funds(rcp = "rcp85", aggregated_regions = aggregated_regions, fuel = "OTHERIND_electricity")
+# ggsave(y, file = '/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/FUND_elec_impactpc.png')
+
+# # get a csv of the damages, for ashwin: 
+# get_and_name = function(rcp, region) {
+# 	args = list(
+# 		    conda_env = "risingverse-py27",
+# 		    proj_mode = '', # '' and _dm are the two options
+# 		    # region = "global", # needs to be specified for 
+# 		    rcp = rcp, 
+# 		    ssp = "SSP3", 
+# 		    price_scen = "price014", # have this as NULL, "price014", "MERGEETL", ...
+# 		    unit =  "damage", # 'damagepc' ($ pc) 'impactpc' (kwh pc) 'damage' ($ pc)
+# 		    uncertainty = "climate", # full, climate, values
+# 		    geo_level = "aggregated", # aggregated (ir agglomerations) or 'levels' (single irs)
+# 		    iam = "high", 
+# 		    model = "TINV_clim", 
+# 		    adapt_scen = "fulladapt", 
+# 		    clim_data = "GMFD", 
+# 		    dollar_convert = "yes", 
+# 		    yearlist = as.character(seq(1980,2100,1)),  
+# 		    spec = "OTHERIND_total_energy",
+# 		    grouping_test = "semi-parametric")
+	
+# 	df = do.call(load.median, c(args, region = region)) %>% 
+# 	                        dplyr::select(mean)
+# 	names(df) = c(region)
+# 	return(df)
+# }
+
+# save_csv = function(rcp) {
+# 	df = mapply(get_and_name, region = aggregated_regions, rcp = "rcp45", SIMPLIFY = FALSE) %>%
+# 			bind_cols()
+# 	df$year = seq(1981,2100,1)
+# 	write_csv(df, paste0('/home/liruixue/repos/energy-code-release-2020/figures/referee_comments/FUND/damages-price014-',
+# 		rcp, 'total_energy-SSP3-high.csv'))
+# 	return(df)
+# }
+# y = save_csv(rcp = "rcp45")
+# z = save_csv(rcp = "rcp85")
+
 
 
