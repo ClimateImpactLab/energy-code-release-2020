5457f744ba541667b68bedb6e86d7bde6d6f9e3b
Ruixue Li <liruixue@uchicago.edu>
2021-05-08 03:33:11 +0800

    done resampling

--- 4_misc/integration/take_draws.R
+++ 4_misc/integration/take_draws.R
@@ -7,25 +7,15 @@ rm(list = ls())
 if(!require("pacman")){install.packages(("pacman"))}
 pacman::p_load(dplyr,
                readr, tidyr)
+source(glue("~/repos/mortality/utils/wrap_mapply.R"))
 
 library(vroom)
 
 source(paste0("~/repos/energy-code-release-2020/2_projection/",
     "0_packages_programs_inputs/extract_projection_outputs/processing_debugging_package.R"))
+input_dir = "/shares/gcp/social/parameters/energy_pixel_interaction/extraction/multi-models/rationalized_code/break2_Exclude_all-issues_semi-parametric/TINV_clim_GMFD/total_energy/price014/"
+output_dir = "/shares/gcp/outputs/energy_pixel_interaction/impacts-blueghost/integration_resampled/"
 
-
-# Set paths
-DB = "/mnt/"
-DB_data = paste0(DB, "/CIL_energy/code_release_data_pixel_interaction")
-dir = paste0(DB_data, "/projection_system_outputs/integration/")
-
-
-# This function takes in a csv that contains means and variances of 
-# each GCMs projected global impact for a given year. 
-# Outputs a long dataframe for damage function estimation, plotting, and 
-# uncertainty calculations
-
-input_path = "/shares/gcp/social/parameters/energy_pixel_interaction/extraction/multi-models/rationalized_code/break2_Exclude_all-issues_semi-parametric/TINV_clim_GMFD/total_energy/price014/"
 projection_path = "/shares/gcp/outputs/energy_pixel_interaction/impacts-blueghost/median_OTHERIND_electricity_TINV_clim_GMFD/median/"
 
 gcms = list()
@@ -38,58 +28,17 @@ gcms$"rcp85" = c(list.dirs(path = paste0(projection_path, "rcp85/"),
   full.names = FALSE,
   recursive = FALSE))
 
-save_files <- function(ssp, rcp, dir) {
-  dt_mean = vroom(glue("{path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_press.csv"))
-  dt_sd = vroom(paste0(path, "SSP3-rcp85_damage-price014_median_fulladapt-levels_press.csv"))
 
 
-}
-
-ssp = "SSP3"
-rcp = "rcp45"
-dt_mean = vroom(glue("{input_path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_dm_press.csv"))
-dt_sd = vroom(glue("{input_path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_dm_press.csv"))
-
-dt_mean_gcms = split(dt_mean , f = dt_mean$gcm)
-dt_sd_gcms = split(dt_sd , f = dt_sd$gcm)
+# This function takes in a csv that contains means and variances of 
+# each GCMs projected global impact for a given year. 
+# Outputs a long dataframe for damage function estimation, plotting, and 
+# uncertainty calculations
 
 
-dt45_mean = dt45_mean %>%
-    rename(mean=value) %>% 
-    dplyr::select(region, year, gcm, iam, mean) %>%  
-    mutate(mean = mean / 0.0036, rcp = "rcp45")
 
-dt45_sd = dt45_sd %>% 
-      mutate(sd=sqrt(value))%>% 
-      dplyr::select(region, year, gcm, iam, sd) %>% 
-      mutate(sd = sd / 0.0036, rcp = "rcp45")
-
-
-dt85_mean = dt85_mean %>%
-    rename(mean=value) %>% 
-    dplyr::select(region, year, gcm, iam, mean) %>%  
-    mutate(mean = mean / 0.0036, rcp = "rcp85")
-
-dt85_sd = dt85_sd %>% 
-      mutate(sd=sqrt(value))%>% 
-      dplyr::select(region, year, gcm, iam, sd) %>% 
-      mutate(sd = sd / 0.0036, rcp = "rcp85")
-
-
-save_gcm <- function(df_mean, df_var, gcm, ssp, rcp, dir) {
-  df
-}
-df_joined = left_join(dt45_mean, dt45_sd, by=c("region", "year", "rcp", "gcm", "iam"))
-
-
-output = paste0('/mnt/CIL_energy/', 
-  '/code_release_data_pixel_interaction/projection_system_outputs/damage_function_estimation')
-
-write_csv(df_joined, paste0(output, '/impact_values/SSP3_TINV_clim_price014_total_energy_mean_sd_valuescsv.csv'))
-
-
-take_draws = function(df, price, fuel, num_iterations, 
-                      directory) {
+take_draws = function(df, num_iterations, 
+                      directory, filename_prefix) {
   
   # set seed for replicability:
   set.seed(123)
@@ -107,20 +56,65 @@ take_draws = function(df, price, fuel, num_iterations,
     dplyr::select(-c(mean, sd)) %>%
     tidyr::pivot_longer(
       cols = starts_with("batch"), 
-      names_to = "batch", 
-      values_to = paste0(price, "value"),
+      names_to = "batch",
       values_drop_na = FALSE)
 
-  write_csv(df, paste0(directory, "resampled_data/",
-                       "SSP3_TINV_clim_price014_total_energy_", num_iterations, "-draws.csv"))
+  write_csv(df, paste0(directory, filename_prefix, 
+                       "_TINV_clim_price014_total_energy_", num_iterations, "-draws.csv"))
 
   return(df)
     
 }
 
+process_results <- function(input_dir, output_dir, gcm, ssp, rcp, iam, num_iterations=15) {
+  mean = vroom(glue("{input_dir}/{ssp}-{rcp}_{iam}_{gcm}_damage-price014_median_fulladapt-levels_press.csv"))
+  sd = vroom(glue("{input_dir}/{ssp}-{rcp}_{iam}_{gcm}_damage-price014_median_fulladapt-levels_dm_press.csv"))
+
+  mean = mean %>%
+      rename(mean=value) %>% 
+      dplyr::select(region, year, mean) %>%  
+      mutate(mean = mean / 0.0036)
+
+  sd = sd %>% 
+        mutate(sd=sqrt(value))%>% 
+        dplyr::select(region, year, sd) %>% 
+        mutate(sd = sd / 0.0036)
+
+  # browser()
+  joined = left_join(mean, sd, by=c("region", "year"))
+  
+  filename_prefix = glue("{ssp}-{rcp}_{iam}_{gcm}")
+  take_draws(joined, num_iterations, output_dir, filename_prefix)
+
+}
+
 ####################
 
-df = take_draws(df_joined, price = "price014",  
-                num_iterations = 15, directory = dir)
+# df = process_results(input_dir=input_dir, output_dir=output_dir, 
+#   gcm="CCSM4", ssp="SSP3", rcp="rcp85", iam="high", num_iterations=15)
 
+
+out = wrap_mapply(  
+  input_dir=input_dir, 
+  output_dir=output_dir, 
+  gcm = gcms$rcp45,
+  rcp="rcp45",
+  iam = c("high","low"),
+  ssp = c("SSP3"),
+  FUN=process_results,
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+out = wrap_mapply(  
+  input_dir=input_dir, 
+  output_dir=output_dir, 
+  gcm = gcms$rcp85,
+  rcp="rcp85",
+  iam = c("high","low"),
+  ssp = c("SSP3"),
+  FUN=process_results,
+  mc.cores=34,
+  mc.silent=FALSE
+)
 
