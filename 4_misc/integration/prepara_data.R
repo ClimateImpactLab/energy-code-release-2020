84df51c00494939967f508a6a55413dc3c0b570a
Ruixue Li <liruixue@uchicago.edu>
2021-05-08 01:55:16 +0800

    parallalized extraction and resampling

--- 4_misc/integration/prepara_data.R
+++ 4_misc/integration/prepara_data.R
@@ -28,31 +28,159 @@ projection.packages <- paste0(REPO,
 	"/energy-code-release-2020/2_projection/0_packages_programs_inputs/extract_projection_outputs/")
 miceadds::source.all(paste0(projection.packages,"load_projection/"))
 
+source(glue("{REPO}/mortality/utils/wrap_mapply.R"))
+
+projection_path = "/shares/gcp/outputs/energy_pixel_interaction/impacts-blueghost/median_OTHERIND_electricity_TINV_clim_GMFD/median/"
+
+gcms = list()
+
+gcms$"rcp45" = c(list.dirs(path = paste0(projection_path, "rcp45/"), 
+  full.names = FALSE,
+  recursive = FALSE))
+
+gcms$"rcp85" = c(list.dirs(path = paste0(projection_path, "rcp85/"), 
+  full.names = FALSE,
+  recursive = FALSE))
+
+
+setwd("~/repos/prospectus-tools/gcp/extract")
+
+extract_file <- function(gcm, ssp, rcp, dm, iam) {
+	# browser()
+
+	command = paste0("nohup python -u quantiles.py ",
+		"/home/liruixue/repos/energy-code-release-2020",
+		"/projection_inputs/configs/GMFD/TINV_clim/",
+		"break2_Exclude/semi-parametric/Extraction_Configs",
+		"/sacagawea/damage/price014/values_press/levels/",
+		"median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy", dm, ".yml  ",
+		"--only-ssp=", ssp, "  --only-rcp=", rcp, "  --only-models=", gcm, 
+		"  --only-iam=",iam, "  --do-gcmweights=no  ",
+		"--suffix=_",iam, "_", gcm, "_damage-price014_median_fulladapt-levels", dm, "_press ",
+		"FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels ",
+		"-FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels ",
+		"FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels ",
+		"-FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels ")
+
+	print(command)
+	system(command)
+}
+
+out = wrap_mapply(  
+  gcm = gcms$rcp45,
+  rcp="rcp45",
+  iam = c("high","low"),
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  FUN=extract_file,
+  dm = "",
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+out = wrap_mapply(  
+  gcm = gcms$rcp85,
+  rcp="rcp85",
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  dm = "",
+  iam = c("high","low"),
+  FUN=extract_file,
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+
+out = wrap_mapply(  
+  gcm = gcms$rcp45,
+  rcp="rcp45",
+  iam = c("high","low"),
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  FUN=extract_file,
+  dm = "_dm",
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+out = wrap_mapply(  
+  gcm = gcms$rcp85,
+  rcp="rcp85",
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  dm = "_dm",
+  iam = c("high","low"),
+  FUN=extract_file,
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+
+out = wrap_mapply(  
+  gcm = gcms$rcp45,
+  rcp="rcp45",
+  iam = c("high","low"),
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  FUN=extract_file,
+  dm = "_dm",
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+
+out = wrap_mapply(  
+  gcm = gcms$rcp85,
+  rcp="rcp85",
+  iam = c("high","low"),
+  ssp = c("SSP1","SSP2","SSP3","SSP4","SSP5"),
+  FUN=extract_file,
+  dm = "_dm",
+  mc.cores=34,
+  mc.silent=FALSE
+)
+
+
+# extract_file(gcm = "surrogate_GFDL-CM3_89", ssp = "SSP3", rcp = "rcp45")
 
 
 
-# rcp85
-python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp85 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
-# rcp45
-python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp45 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
+# ssp = "SSP3"
+# rcp = "rcp45"
+# gcm = "CanESM2"
+# command = paste0("nohup python -u quantiles.py ",
+# 	"/home/liruixue/repos/energy-code-release-2020",
+# 	"/projection_inputs/configs/GMFD/TINV_clim/",
+# 	"break2_Exclude/semi-parametric/Extraction_Configs",
+# 	"/sacagawea/damage/price014/values_press/levels/",
+# 	"median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  ",
+# 	"--only-ssp=", ssp, "  --only-rcp=", rcp, "  --only-models=", gcm, 
+# 	"  --do-gcmweights=no  ",
+# 	"--suffix=_", gcm, "_damage-price014_median_fulladapt-levels_press ",
+# 	"FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels ",
+# 	"-FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels ",
+# 	"FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels ",
+# 	"-FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels ")
 
-# _dm
-# rcp85
-python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy_dm.yml  --only-ssp=SSP3   --only-rcp=rcp85 --suffix=_damage-price014_median_fulladapt-levels_dm_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
-# rcp45
-python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy_dm.yml  --only-ssp=SSP3   --only-rcp=rcp45 --suffix=_damage-price014_median_fulladapt-levels_dm_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
 
 
-if (price %in% c("price014", "price0", "price03")) {
+# python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp85 --only-models=CanESM2 --do-gcmweights=no --only-iam=high  --suffix=_CanESM2_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
 
-    mean = do.call(load.median, c(args, proj_mode = '')) %>%
-		rename(mean=value) %>% 
-		dplyr::select(rcp, year, region, gcm, iam, mean) %>% 
-		mutate(mean = mean / 0.0036)
 
-	if(include_variance == TRUE){	
-		var = do.call(load.median, c(args, proj_mode = '_dm')) %>% 
-			mutate(sd=sqrt(value))%>% 
-			dplyr::select(rcp, year, region, gcm, iam, sd) %>% 
-			mutate(sd = sd / 0.0036)
-	}
+# # bash script:
+# for ssp in {1..5}
+# do 
+# 	# python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp85 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
+# 	python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp45 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
+# done 
+
+# # run under risingverse-py27 environment, prospectus-tools/gcp/extract directory
+# # # rcp85
+# # python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp85 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
+# # # rcp45
+# # python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy.yml  --only-ssp=SSP3  --only-rcp=rcp45 --suffix=_damage-price014_median_fulladapt-levels_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels 
+
+# # # _dm
+# # # rcp85
+# # python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy_dm.yml  --only-ssp=SSP3   --only-rcp=rcp85 --suffix=_damage-price014_median_fulladapt-levels_dm_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
+# # # rcp45
+# # # 
+# python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy_dm.yml --only-ssp=SSP3   --only-rcp=rcp45 --suffix=_damage-price014_median_fulladapt-levels_dm_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
+
+
+# python -u quantiles.py /home/liruixue/repos/energy-code-release-2020/projection_inputs/configs/GMFD/TINV_clim/break2_Exclude/semi-parametric/Extraction_Configs/sacagawea/damage/price014/values_press/levels/median/energy-extract-damage-levels-price014-median_OTHERIND_total_energy_dm.yml  --only-ssp=SSP3  --only-rcp=rcp85  --only-models=CCSM4  --only-iam=high  --do-gcmweights=no  --suffix=_low_CanESM2_damage-price014_median_fulladapt-levels_dm_press FD_FGLS_inter_OTHERIND_electricity_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_electricity_TINV_clim-histclim-price014-levels FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-price014-levels -FD_FGLS_inter_OTHERIND_other_energy_TINV_clim-histclim-price014-levels
--- 4_misc/integration/take_draws.R
+++ 4_misc/integration/take_draws.R
@@ -8,11 +8,16 @@ if(!require("pacman")){install.packages(("pacman"))}
 pacman::p_load(dplyr,
                readr, tidyr)
 
+library(vroom)
+
+source(paste0("~/repos/energy-code-release-2020/2_projection/",
+    "0_packages_programs_inputs/extract_projection_outputs/processing_debugging_package.R"))
+
 
 # Set paths
 DB = "/mnt/"
 DB_data = paste0(DB, "/CIL_energy/code_release_data_pixel_interaction")
-dir = paste0(DB_data, "/projection_system_outputs/damage_function_estimation/")
+dir = paste0(DB_data, "/projection_system_outputs/integration/")
 
 
 # This function takes in a csv that contains means and variances of 
@@ -20,26 +25,74 @@ dir = paste0(DB_data, "/projection_system_outputs/damage_function_estimation/")
 # Outputs a long dataframe for damage function estimation, plotting, and 
 # uncertainty calculations
 
+input_path = "/shares/gcp/social/parameters/energy_pixel_interaction/extraction/multi-models/rationalized_code/break2_Exclude_all-issues_semi-parametric/TINV_clim_GMFD/total_energy/price014/"
+projection_path = "/shares/gcp/outputs/energy_pixel_interaction/impacts-blueghost/median_OTHERIND_electricity_TINV_clim_GMFD/median/"
 
-take_draws = function(price, ssp, fuel, num_iterations, 
+gcms = list()
+
+gcms$"rcp45" = c(list.dirs(path = paste0(projection_path, "rcp45/"), 
+  full.names = FALSE,
+  recursive = FALSE))
+
+gcms$"rcp85" = c(list.dirs(path = paste0(projection_path, "rcp85/"), 
+  full.names = FALSE,
+  recursive = FALSE))
+
+save_files <- function(ssp, rcp, dir) {
+  dt_mean = vroom(glue("{path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_press.csv"))
+  dt_sd = vroom(paste0(path, "SSP3-rcp85_damage-price014_median_fulladapt-levels_press.csv"))
+
+
+}
+
+ssp = "SSP3"
+rcp = "rcp45"
+dt_mean = vroom(glue("{input_path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_dm_press.csv"))
+dt_sd = vroom(glue("{input_path}/{ssp}-{rcp}_damage-price014_median_fulladapt-levels_dm_press.csv"))
+
+dt_mean_gcms = split(dt_mean , f = dt_mean$gcm)
+dt_sd_gcms = split(dt_sd , f = dt_sd$gcm)
+
+
+dt45_mean = dt45_mean %>%
+    rename(mean=value) %>% 
+    dplyr::select(region, year, gcm, iam, mean) %>%  
+    mutate(mean = mean / 0.0036, rcp = "rcp45")
+
+dt45_sd = dt45_sd %>% 
+      mutate(sd=sqrt(value))%>% 
+      dplyr::select(region, year, gcm, iam, sd) %>% 
+      mutate(sd = sd / 0.0036, rcp = "rcp45")
+
+
+dt85_mean = dt85_mean %>%
+    rename(mean=value) %>% 
+    dplyr::select(region, year, gcm, iam, mean) %>%  
+    mutate(mean = mean / 0.0036, rcp = "rcp85")
+
+dt85_sd = dt85_sd %>% 
+      mutate(sd=sqrt(value))%>% 
+      dplyr::select(region, year, gcm, iam, sd) %>% 
+      mutate(sd = sd / 0.0036, rcp = "rcp85")
+
+
+save_gcm <- function(df_mean, df_var, gcm, ssp, rcp, dir) {
+  df
+}
+df_joined = left_join(dt45_mean, dt45_sd, by=c("region", "year", "rcp", "gcm", "iam"))
+
+
+output = paste0('/mnt/CIL_energy/', 
+  '/code_release_data_pixel_interaction/projection_system_outputs/damage_function_estimation')
+
+write_csv(df_joined, paste0(output, '/impact_values/SSP3_TINV_clim_price014_total_energy_mean_sd_valuescsv.csv'))
+
+
+take_draws = function(df, price, fuel, num_iterations, 
                       directory) {
   
   # set seed for replicability:
   set.seed(123)
-  
-  # Set up strings used for loading in the data
-  if(is.null(price)){
-    type = "impacts"
-    price_tag = ""
-  }else{
-    type = "damages"
-    price_tag = paste0("_", price)
-  }
-  
-  # Read in the gcm level means and standard deviations
-  df = read_csv(paste0(directory, "impact_values/",
-          "gcm_", type, "_OTHERIND_",fuel ,price_tag, "_",ssp, ".csv"))
-  
   
   # Take draws
   l = length(df$mean)
@@ -59,34 +112,15 @@ take_draws = function(price, ssp, fuel, num_iterations,
       values_drop_na = FALSE)
 
   write_csv(df, paste0(directory, "resampled_data/",
-                       "gcm_", type, "_OTHERIND_",fuel,
-                       price_tag, "_",ssp,"-", num_iterations, "-draws.csv"))
+                       "SSP3_TINV_clim_price014_total_energy_", num_iterations, "-draws.csv"))
 
   return(df)
     
 }
 
 ####################
-# 1. Get the data needed for the display in Figure 3C
 
-df = take_draws(price = "price014", ssp = "SSP3", 
-                fuel = "total_energy", num_iterations = 15, directory = dir)
-
-df_oe = take_draws(price = NULL, ssp = "SSP3", 
-                fuel = "other_energy", num_iterations = 15, directory = dir)
-
-df_elec = take_draws(price = NULL, ssp = "SSP3", 
-                fuel = "electricity", num_iterations = 15, directory = dir)
-
-
-
-####################
-# 2. Take 100 draws from all price scenarios, for use in quantile regressions
-pricelist = c("price014", "price0", "price03", "WITCHGLOBIOM42", 
-              "MERGEETL60", "REMINDMAgPIE1730", "REMIND17CEMICS", "REMIND17") 
-
-lapply(pricelist, FUN = take_draws, 
-       ssp = "SSP3", fuel = "total_energy", num_iterations = 100, directory = dir)
-
+df = take_draws(df_joined, price = "price014",  
+                num_iterations = 15, directory = dir)
 
 
