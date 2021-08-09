a17e33a7d04783b1027c06fac3b1514ad0222513
Ruixue Lui <liruixue@battuta.berkeley.edu>
2021-08-09 07:27:12 -0700

    log

--- 0_make_dataset/0_make_dataset.sh
+++ 0_make_dataset/0_make_dataset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "STEP 1"
-stata-mp  < 1_construct_dataset_from_raw_inputs.do > $LOG/1_construct_dataset_from_raw_inputs.log &
+stata-mp -b do 1_construct_dataset_from_raw_inputs.do 
 echo "STEP 2"
-stata-mp  < 2_construct_regression_ready_data.do > $LOG/2_construct_regression_ready_data.log &
+stata-mp -b  do 2_construct_regression_ready_data.do 
 echo "STEP 3"
-stata-mp  < 3_unit_root_test_and_plot.do > $LOG/3_unit_root_test_and_plot.log &
+stata-mp  -b do 3_unit_root_test_and_plot.do 
 echo "STEP 4"
-Rscript 4_plot_ITA_other_energy_regimes_timeseries.R 2 > $LOG/4_plot_ITA_other_energy_regimes_timeseries.log &
+Rscript 4_plot_ITA_other_energy_regimes_timeseries.R > $LOG/4_plot_ITA_other_energy_regimes_timeseries.log &
 
