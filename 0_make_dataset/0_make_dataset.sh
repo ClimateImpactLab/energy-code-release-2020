cce487f78ea6322c8fff756de18a9cf485f6a5ce
Ruixue Li <liruixue@uchicago.edu>
2021-08-09 22:07:25 +0800

    log

--- 0_make_dataset/0_make_dataset.sh
+++ 0_make_dataset/0_make_dataset.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "STEP 1"
-stata-mp  do 1_construct_dataset_from_raw_inputs.do
+stata-mp  < 1_construct_dataset_from_raw_inputs.do > $LOG/1_construct_dataset_from_raw_inputs.log &
 echo "STEP 2"
-stata-mp  do 2_construct_regression_ready_data.do
+stata-mp  < 2_construct_regression_ready_data.do > $LOG/2_construct_regression_ready_data.log &
 echo "STEP 3"
-stata-mp  do 3_unit_root_test_and_plot.do
+stata-mp  < 3_unit_root_test_and_plot.do > $LOG/3_unit_root_test_and_plot.log &
 echo "STEP 4"
-Rscript 4_plot_ITA_other_energy_regimes_timeseries.R
+Rscript 4_plot_ITA_other_energy_regimes_timeseries.R 2 > $LOG/4_plot_ITA_other_energy_regimes_timeseries.log &
+
--- setup_paths.sh
+++ setup_paths.sh
@@ -3,5 +3,6 @@
 export REPO=/home/liruixue/repos_release/
 export DATA=/home/liruixue/energy_data_release/DATA
 export OUTPUT=/home/liruixue/energy_data_release/OUTPUT
+export LOG=/home/liruixue/repos_release/LOG
 
 conda activate risingverse
