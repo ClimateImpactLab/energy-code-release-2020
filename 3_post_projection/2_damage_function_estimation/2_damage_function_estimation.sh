e6a45f23b4e7ae054c8156a3dea416301c8cdaa7
Ruixue Li <liruixue@uchicago.edu>
2021-10-18 23:40:19 +0800

    removed log

--- 3_post_projection/2_damage_function_estimation/2_damage_function_estimation.sh
+++ 3_post_projection/2_damage_function_estimation/2_damage_function_estimation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 Rscript 1_take_draws.R
-stata-se -b plot_city_responses.do
-Rscript plot_damages_by_2012_income_decile.R
-Rscript plot_kernel_density_function.R
-Rscript plot_maps.R
-Rscript plot_time_series.R
+stata-se -b do 2_plot_damage_function_fig_3.do
+stata-se -b do 3_run_damage_functions.do 
+stata-se -b do 4_run_quantile_regressions.do 
+stata-se -b do 5_plot_damage_function_over_time.do 
+stata-se -b do 6_get_end_of_century_df_slopes_p_vals.do 
